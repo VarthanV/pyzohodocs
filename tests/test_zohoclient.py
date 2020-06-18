466eed5c9cb21c11e483a66df84b06619b22c34a
Vishnu Varthan <vishnulatha006@gmail.com>
2020-06-18 20:09:25 +0530

    Added Relavant Tests

--- tests/test_zohoclient.py
+++ tests/test_zohoclient.py
@@ -5,16 +5,15 @@ def test_auth_fails():
     with pytest.raises(Exception) as e :
         z =ZohoAuth.get_auth_token('v','dd','cc')
 
-def test_upload_docs_client_fails():
+def test_upload_docs_client_fails_when_key_invalid():
     with pytest.raises(Exception) as e:
         client = ZohoDocsClient('attjtj')
         client.upload_file(file_name='examplee.docx',file_path = 'The path to your file')
 
-def test_download_docs_fails():
+def test_download_docs_fails_when_key_invalid():
     with pytest.raises(Exception) as e:
         client = ZohoDocsClient('attjtj')
         client.download_file('dkdjd',file_name='v.docx')
-
 
 
 
